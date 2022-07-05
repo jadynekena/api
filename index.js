98d3b3e4fc6b9aaa70b12fb7b0d578ff4664ab65
jadynekena <jadynekena@yahoo.com>
2022-07-05 05:16:56 +0200

    nothing

--- index.js
+++
@@ -1,9 +0,0 @@
-//TO USE THIS API, PLEASE FOLLOW THE INSTRUCTIONS BELOW.
-//EXAMPLE : eval(await $.get('https://api.jadynekena.com/index.js?param1=val1&parm2=val2'))
-function results(){
-	url = localStorage['url']
-	localStorage.clear()
-
-	return url
-}
-results().toString()

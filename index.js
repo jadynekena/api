420209f7656a03678b58cb17e407753c35719f7f
jadynekena <jadynekena@yahoo.com>
2022-07-05 05:05:16 +0200

    no return

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //TO USE THIS API, PLEASE FOLLOW THE INSTRUCTIONS BELOW.
 //EXAMPLE : res = $.get('http://api.jadynekena.com/index.js?param=0'); final_res=eval(res.body.innerText);
 
-return results().toString()
+results().toString()
 function results(){
 	url = localStorage['url']
 	localStorage.clear()

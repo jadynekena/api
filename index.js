c9d84efb228a369ad6dcded343a6cef8c7d87de2
jadynekena <jadynekena@yahoo.com>
2022-07-05 05:06:46 +0200

    result undefined

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 //TO USE THIS API, PLEASE FOLLOW THE INSTRUCTIONS BELOW.
 //EXAMPLE : eval(await $.get('https://api.jadynekena.com/index.js?param1=val1&parm2=val2'))
-
-results().toString()
 function results(){
 	url = localStorage['url']
 	localStorage.clear()
 
 	return url
 }
+results().toString()
